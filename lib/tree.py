46ee60c503471bd27d2bfac49eeebb02d64a2f82
Trevor <nyigeitrevor@gmail.com>
2025-06-01 18:20:50 +0300

    fin

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def dfs(node):
+            if node['id'] == id:
+                return node
+            for child in node.get('children', []):
+                found = dfs(child)
+                if found:
+                    return found
+            return None
+        
+        if self.root is None:
+            return None
+        return dfs(self.root)
